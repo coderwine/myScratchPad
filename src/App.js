fa7f5c0d2c004f1c7bfecc37e7d3f0ddd060f6fd
Eric <ejwinebrenner@gmail.com>
2021-11-01 20:38:52 -0400

    initial push

---
+++ src/App.js
@@ -0,0 +1,15 @@
+// import logo from './logo.svg';
+import './App.css';
+
+// Components
+import Cycle from './components/Cycle'
+
+function App() {
+  return (
+    <div className="App">
+        <Cycle />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Cycle.jsx
@@ -0,0 +1,11 @@
+
+export default function Cycle (props) {
+
+    // I'd like to try out some SCSS
+    return(
+        <>
+            <h1>Cycle Titles</h1>
+            
+        </>
+    )
+}
