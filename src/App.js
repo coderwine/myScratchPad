2b87a7d8f1194208bbe163cdf4cd1beb760002f6
Eric <ejwinebrenner@gmail.com>
2021-11-18 16:35:51 -0500

    installed v6 Router Dom for practice later

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import MainPage from './components/MainPage'
 import Cycle from './components/Cycle'
 
 function App() {
+  //NOTE:  React Router Dom v6 installed.  Would like to build out a proper nav to swap between my scratch pad.
+  //NOTE: Would like to look into incorporating next.js or Redux in order to handle state management.
   return (
     <div className="App">
         <Navbar />
